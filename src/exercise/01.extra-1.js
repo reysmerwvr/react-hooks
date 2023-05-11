c1710964e9da7d283a27bce9be5831c1812d8622
Reysmer Valle <reysmerwvr@gmail.com>
2023-05-11 22:07:13 +0200

    feat(react-hooks): implement useState hook

--- src/exercise/01.extra-1.js
+++ src/exercise/01.extra-1.js
@@ -3,8 +3,8 @@
 
 import * as React from 'react'
 
-function Greeting() {
-  const [name, setName] = React.useState('')
+function Greeting({initialName = ''}) {
+  const [name, setName] = React.useState(initialName)
 
   function handleChange(event) {
     const {
@@ -17,7 +17,7 @@ function Greeting() {
     <div>
       <form>
         <label htmlFor="name">Name: </label>
-        <input onChange={handleChange} id="name" />
+        <input value={name} onChange={handleChange} id="name" />
       </form>
       {name ? <strong>Hello {name}</strong> : 'Please type your name'}
     </div>
@@ -25,7 +25,7 @@ function Greeting() {
 }
 
 function App() {
-  return <Greeting />
+  return <Greeting initialName="Max" />
 }
 
 export default App
